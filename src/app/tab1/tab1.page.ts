0c4d042c6224362c05181ce55e5e6a0d06c763a3
KilliBruhh <61047380+KilliBruhh@users.noreply.github.com>
2022-12-22 14:47:53 +0100

    Added electrons tuff to .ts file

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { NgZone } from '@angular/core';
+
 
 import { BackendService } from '../backend.service';
 
@@ -8,6 +10,10 @@ import { BackendService } from '../backend.service';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })  
+
+
+
+
 export class Tab1Page {
 
   drink!: string;
@@ -18,7 +24,27 @@ export class Tab1Page {
   showMsg: boolean = false;
 
 
-  constructor(private bs: BackendService) {}
+  
+
+  reply = '';
+  electronVersion = '';
+  constructor(private ngZone: NgZone, private bs: BackendService) {
+      this.electronVersion = window.api.getElectronVersion();
+      window.api.ipcSendToMain();
+      window.api.ipcReceiveReplyFromMain('do-a-thing-reply',(event: any, arg: any) => {
+      console.log(arg);
+      //Because the event handlers are called outside the context of Angular
+      //Angular Can't do "change detection" and the html won't be updated if the variables
+      //are changed. (Here reply via string interpolation in the html)
+      //Use the 2 lines below to show that the value has been changed but without the
+      //ngZone.run the new content will not be visible in the html
+      //this.reply = arg;
+      //console.log(this.reply);
+      this.ngZone.run(() => {
+          this.reply = arg;
+      });
+      });
+  }
 
   onAddTurf(form:NgForm) {
     if(!form.valid) {
